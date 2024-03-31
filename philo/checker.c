baf0b4f18354cbae56c60524738fd89c98beff7d
Ayoub Bablil <ayoubbablil@gmail.com>
2024-03-31 07:54:52 +0000

    New update

--- philo/checker.c
+++ philo/checker.c
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 17:58:39 by abablil           #+#    #+#             */
-/*   Updated: 2024/03/30 22:26:37 by abablil          ###   ########.fr       */
+/*   Updated: 2024/03/31 07:50:38 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@ int	checker(t_data *data)
 	long long	death_time;
 	int			is_full;
 	int			i;
-	int			total_finished;
+	int			total_full;
 
-	(1) && (i = -1, total_finished = 0);
+	(1) && (i = -1, total_full = 0);
 	while (++i < data->n_philos)
 	{
 		pthread_mutex_lock(&data->philos[i].philo_lock);
 		death_time = data->philos[i].death_time;
 		is_full = data->philos[i].is_full;
 		pthread_mutex_unlock(&data->philos[i].philo_lock);
-		if (get_time() >= death_time && death_time && !is_full)
+		if (death_time && get_time() >= death_time && !is_full)
 		{
 			print(&data->philos[i], DEAD);
 			return (0);
 		}
-		total_finished += is_full;
-		if (total_finished == data->n_philos)
+		total_full += is_full;
+		if (total_full == data->n_philos)
 		{
 			end_simulation(data);
 			return (0);
@@ -62,9 +62,9 @@ int	checker_monitor(t_data *data)
 	i = -1;
 	if (pthread_create(&data->monitor_thread, NULL,
 			checker_routine, (void *) data))
-		return (1);
+		return (-1);
 	pthread_join(data->monitor_thread, NULL);
 	while (++i < data->n_philos)
 		pthread_join(data->philos[i].thread, NULL);
-	return (0);
+	return (1);
 }
