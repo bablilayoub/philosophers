7f64e38f26503e87c4bd79f93b7ec1a55474e0c7
Ayoub Bablil <ayoubbablil@gmail.com>
2024-04-16 19:37:00 +0100

    New update

--- philo_bonus/checker.c
+++ philo_bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 11:01:50 by abablil           #+#    #+#             */
-/*   Updated: 2024/04/16 11:16:09 by abablil          ###   ########.fr       */
+/*   Updated: 2024/04/16 19:33:09 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,18 @@ void	*check_death(void *args)
 	long long	current_time;
 
 	data = (t_data *)args;
-	current_time = get_time();
 	while (1)
 	{
+		current_time = get_time();
 		if (current_time - data->last_time_eat > data->time_to_die)
 		{
 			sem_wait(data->print);
 			printf("%lld %ld died\n", current_time - data->start_time,
 				data->philo_id);
-			sem_post(data->print);
 			exit(1);
 		}
 		if (data->n_times_to_eat != -1 && data->meals >= data->n_times_to_eat)
-		{
-			sem_post(data->forks);
-			sem_post(data->forks);
 			exit(0);
-		}
 	}
 	return (NULL);
 }
